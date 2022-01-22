8ea9d0b9deb3620e8ea8e02b2be4fb15fc3de038
Mohamed A. Ibrahim <rageali12@gmail.com>
2022-01-22 23:28:07 +0300

    fixed some bugs and created a new Behavior that works

--- src/main/java/Encryption.java
+++ src/main/java/Encryption.java
@@ -1,2 +1,61 @@
-package PACKAGE_NAME;public class Encryption {
+public class Encryption {
+
+    private String mInputString;
+    private int mShift;
+    //    gettters
+
+    public String getmInputString(){
+        return mInputString;
+    }
+
+    public int getmShift(){
+        return mShift;
+    }
+
+    //constructors
+    public Encryption(String mInputString, int mShift) {
+        this.mInputString = mInputString;
+        this.mShift = mShift;
+    }
+    public static String encrypt(Encryption encrypt){
+
+        if(encrypt.mShift > 26){
+            encrypt.mShift = encrypt.mShift % 26;
+        }
+        else if(encrypt.mShift < 0) {
+            encrypt.mShift = (encrypt.mShift % 26) + 26;
+        }
+
+        String outputString = "";
+        int length = encrypt.mInputString.length();
+        for (int i = 0; i < length ; i++) {
+            char cc = encrypt.mInputString.charAt(i);
+            if(Character.isLetter(cc)){
+                if(Character.isLowerCase(cc)){
+                    char c = (char)(cc+encrypt.mShift);
+                    if(c>'z'){
+                        outputString += (char)(cc - (26-encrypt.mShift));
+                    }
+                    else {
+                        outputString += c;
+                    }
+                }
+                else if(Character.isUpperCase(cc)){
+                    char c = (char)(cc+encrypt.mShift);
+                    if(c>'Z'){
+                        outputString += (char)(cc - (26-encrypt.mShift));
+                    }
+                    else {
+                        outputString += c;
+                    }
+                }
+            }
+            else{
+                outputString += cc;
+            }
+        }
+        return outputString;
+    }
+
 }
+
